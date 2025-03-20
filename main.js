c023d6becd72147223368d3166231a5eac54b065
Samuel Uzelino <uzelino.sam@gmail.com>
2025-03-20 18:12:18 +0100

    Js formatieren

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+var map = L.map('map').setView([-39.48, 176.90], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([-39.48, 176.90]).addTo(map);
+marker.bindPopup("<b>Napier</b><br>Center").openPopup();
